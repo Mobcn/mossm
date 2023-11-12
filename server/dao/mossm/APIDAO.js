637c130449b62ab25d27558f269cbaa14e08935b
Mobcn <mobingcn@gmail.com>
2023-11-12 11:10:39 +0800

    初始化

--- server/dao/mossm/APIDAO.js
+++ server/dao/mossm/APIDAO.js
@@ -6,39 +6,7 @@ import { Model } from './model/APIModel.js';
  *
  * @extends {BaseDAO<typeof Model>}
  */
-class APIDAO extends BaseDAO {
-    /**
-     * 获取满足条件的分页数据
-     *
-     * @param {Object} param0 参数
-     * @param {import('mongoose').FilterQuery<import('../BaseDAO').RawDocType<typeof Model>>} [param0.filter={}] 条件
-     * @param {number} [param0.page=1] 页数
-     * @param {number} [param0.limit=10] 每页数据条数
-     */
-    async page({ filter = {}, page = 1, limit = 10 }) {
-        const [list, total] = await Promise.all([
-            this.Model.aggregate([
-                {
-                    $sort: {
-                        module: 1,
-                        model: 1,
-                        path: 1
-                    }
-                },
-                {
-                    $sort: {
-                        create_time: -1
-                    }
-                }
-            ])
-                .skip((page - 1) * limit)
-                .limit(limit)
-                .exec(),
-            this.Model.count(filter)
-        ]);
-        return { list, total };
-    }
-}
+class APIDAO extends BaseDAO {}
 
 const apiDAO = new APIDAO(Model);
 export { APIDAO, apiDAO };
