78aa1ce155f15cdf1c3a7225f16282512b79c3f1
Mobcn <mobingcn@gmail.com>
2024-09-26 15:05:25 +0800

    update:适配Netlify

--- netlify/edge-functions/index.js
+++ netlify/edge-functions/index.js
@@ -7,33 +7,3 @@ const app = new Hono();
 app.all('*', (context) => dispatcher(context));
 
 export default handle(app);
-
-/**
- * 配置
- *
- * @type {Config}
- */
-export const config = {
-  path: '/(.+)/*',
-  excludedPath: ['assets', 'img', 'setting'].map((name) => `/${name}/*`)
-};
-
-/** @typedef {"off" | "manual"} Cache 缓存 */
-/** @typedef {`/${string}`} Path 路径 */
-/** @typedef {"fail" | "bypass" | Path} OnError 错误事件 */
-/** @typedef {"GET" | "POST" | "PUT" | "PATCH" | "DELETE" | "OPTIONS"} HTTPMethod 请求方法 */
-/**
- * @typedef {Object} Context 请求上下文
- * @property {Object} cookies Cookie
- * @property {(name: string) => string} cookies.get 获取
- * @property {(name: string, value: string) => string} cookies.set 设置
- * @property {(name: string) => string} cookies.delete 删除
- */
-/**
- * @typedef {Object} Config 边缘函数配置
- * @property {Cache} [cache] 缓存
- * @property {Path | Path[]} [excludedPath] 排除路径
- * @property {OnError} [onError] 错误事件
- * @property {Path | Path[]} [path] 匹配路径
- * @property {HTTPMethod | HTTPMethod[]} [method] 请求方法
- */
